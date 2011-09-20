db8ed3bfe66ef3f411484a483d9140d5b01d703c
Nate Folkert <nfolkert@foursquare.com>
2011-09-20 07:54:18 -0400

    Add logger and mongo to project

--- project/build/StrategicFoursquare.scala
+++ project/build/StrategicFoursquare.scala
@@ -4,13 +4,20 @@ class ScalaFoursquareProject(info: ProjectInfo) extends DefaultWebProject(info) 
   override def managedStyle = ManagedStyle.Maven
   override def packageSrcJar= defaultJarPath("-sources.jar")
 
-  val liftWebkit = "net.liftweb" %% "lift-webkit" % "2.4-M1" % "compile->default" withSources()
-  val liftMapper = "net.liftweb" %% "lift-mapper" % "2.4-M1" % "compile->default"
+  val liftVer = "2.4-M1"
 
-  val liftCommon = "net.liftweb" %% "lift-common" % "2.4-M1" % "compile" withSources()
-  val liftUtil = "net.liftweb" %% "lift-util" % "2.4-M1" % "compile" withSources()
-  val liftJson = "net.liftweb" %% "lift-json" % "2.4-M1" % "compile" withSources()
-  val scalajCollection  = "org.scalaj" %% "scalaj-collection"  % "1.1"
+  val liftWebkit = "net.liftweb" %% "lift-webkit" % liftVer % "compile->default" withSources()
+  val liftMapper = "net.liftweb" %% "lift-mapper" % liftVer % "compile->default"
+
+  val liftCommon = "net.liftweb" %% "lift-common" % liftVer % "compile" withSources()
+  val liftUtil = "net.liftweb" %% "lift-util" % liftVer % "compile" withSources()
+  val liftJson = "net.liftweb" %% "lift-json" % liftVer % "compile" withSources()
+
+  val liftMongo       = "net.liftweb" %% "lift-mongodb" % liftVer % "compile" withSources() intransitive()
+  val liftMongoRecord = "net.liftweb" %% "lift-mongodb-record" % liftVer % "compile" withSources() intransitive()
+  val mongo        = "org.mongodb" % "mongo-java-driver" % "2.5.3a_fs" withSources()
+
+  val scalajCollection = "org.scalaj" %% "scalaj-collection" % "1.1"
   val scalajHttp = "org.scalaj" %% "scalaj-http" % "0.2.8" % "compile" withSources()
 
   val junit = "junit" % "junit" % "4.8.2" % "test" withSources()
@@ -19,6 +26,8 @@ class ScalaFoursquareProject(info: ProjectInfo) extends DefaultWebProject(info) 
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.25" % "test,provided"
   val jettyutils = "org.mortbay.jetty" % "jetty-util" % "6.1.25" % "test,provided"
+
+  val logback = "ch.qos.logback" % "logback-classic" % "0.9.26"
 
   val h2db = "com.h2database" % "h2" % "1.2.138"
 
